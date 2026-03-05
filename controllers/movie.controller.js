36a4ad0541402f07130e384e909a34fb52e5698e
AmanBhaskar16 <amanbhaskar16@gmail.com>
2026-03-05 13:29:53 +0530

    Validation : Added client side validation checks using zod

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,26 +1,22 @@
-import Movie from "../models/movie.model.js";
-
-import { createMovieService ,deleteMovieService, getMovieService} from "../services/movie.service.js";
+import { createMovieService ,deleteMovieService, fetchMoviesService, getMovieService, updateMovieService, updateMovieStatusService} from "../services/movie.service.js";
 
 import { errorResponseBody,successResponseBody } from "../utils/response.utils.js";
 
-/**
- * Controller function to create a new movie
- * @param {*} req {name,description,duration,...}
- * @param {*} res {success/failure status}
- * @returns // Newly created movie
- */
+import { STATUS_CODES } from "../utils/constants.js";
 
 export const createMovie =  async (req,res) => {
     try {
         const movie = await createMovieService(req.body);
         successResponseBody.message = "Movie added successfully";
         successResponseBody.data = movie;
-        return res.status(201).json(successResponseBody);
-    } catch (err) {
-        console.log(err);
-        errorResponseBody.err = err;
-        return res.status(500).json(errorResponseBody);
+        return res.status(STATUS_CODES.CREATED).json(successResponseBody);
+    } catch (error) {
+        if(error.err) {
+            errorResponseBody.err = error.err;
+            return res.status(error.code).json(errorResponseBody);
+        }
+        errorResponseBody.err = error;
+        return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json(errorResponseBody);
     }
 };
 
@@ -29,11 +25,14 @@ export const deleteMovie = async (req,res) => {
         const response = await deleteMovieService(req.params.id);
         successResponseBody.message = "Successfully deleted the movie.";
         successResponseBody.data = response;
-        return res.status(200).json(successResponseBody);
-    } catch (err) {
-        console.log(err);
-        errorResponseBody.err = err;
-        return res.status(500).json(errorResponseBody);
+        return res.status(STATUS_CODES.OK).json(successResponseBody);
+    } catch (error) {
+        if(error.err) {
+            errorResponseBody.err = error.err;
+            return res.status(error.code).json(errorResponseBody);
+        }
+        errorResponseBody.err = error;
+        return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json(errorResponseBody);
     }
 }
 
@@ -47,10 +46,74 @@ export const getMovie = async (req,res) => {
         }
         successResponseBody.data = movieDetails;
         successResponseBody.message = "Fetched movie details.";
-        return res.status(200).json(successResponseBody);
+        return res.status(STATUS_CODES.OK).json(successResponseBody);
+    } catch (error) {
+        if(error.err) {
+            errorResponseBody.err = error.err;
+            return res.status(error.code).json(errorResponseBody);
+        }
+        errorResponseBody.err = error;
+        return res.status(STATUS.INTERNAL_SERVER_ERROR).json(errorResponseBody);
+    }
+}
+
+export const updateMovie = async (req,res) => {
+    try {
+        const movie = await updateMovieService(req.params.id,req.body);
+        if(movie.err){
+            errorResponseBody.err = movie.err;
+            return res.status(movie.code).json(errorResponseBody);
+        }
+        successResponseBody.data = movie;
+        successResponseBody.message = "Movie updated successfully.";
+        return res.status(STATUS_CODES.OK).json(successResponseBody);
     } catch (err) {
         console.log(err);
         errorResponseBody.err = err;
-        return res.status(500).json(errorResponseBody);
+        return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json(errorResponseBody);
     }
 }
+
+export const updateMovieStatus = async (req, res) => {
+    try {
+        const { isActive } = req.body;
+
+        const movie = await updateMovieStatusService(req.params.id, isActive);
+
+        return res.status(STATUS_CODES.OK).json({
+            success: true,
+            message: "Movie status updated successfully",
+            data: movie
+        });
+
+    } catch (error) {
+
+        if(error.err){
+            return res.status(error.code).json({
+                success: false,
+                error: error.err
+            });
+        }
+
+        return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            error: "Internal server error"
+        });
+    }
+};
+
+export const getMovies = async (req,res) => {
+    try {
+        const response = await fetchMoviesService(req.query);
+        successResponseBody.data = response;
+        return res.status(STATUS_CODES.OK).json(successResponseBody);
+    } catch (error) {
+        if(error.err) {
+            errorResponseBody.err = error.err;
+            return res.status(error.code).json(errorResponseBody);
+        }
+        errorResponseBody.err = error;
+        return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json(errorResponseBody);
+    }
+}
+
--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,10 +1,21 @@
 import express from "express";
-import {createMovie, deleteMovie, getMovie} from "../controllers/movie.controller.js"
+import {createMovie, deleteMovie, getMovie,getMovies,updateMovie, updateMovieStatus} from "../controllers/movie.controller.js"
+import { validate } from "../middlewares/movie.middleware.js";
+import { movieSchema } from "../validations/movie.validation.js";
 
 const router = express.Router();
 
-router.post('/movies',createMovie);
+// CREATE A MOVIE
+router.post('/movies',validate(movieSchema),createMovie);
+// DELETE A MOVIE
 router.delete("/movies/:id",deleteMovie);
+// GET A PARTICULAR MOVIE
 router.get("/movies/:id",getMovie);
+// UPDATE A PARTICULAR MOVIE
+router.patch("/movies/:id",updateMovie);
+// SETUP THE STATUS OF A PARTICULAR MOVIE
+router.patch("/movies/:id/status", updateMovieStatus);
+// GET ALL THE MOVIES
+router.get("/movies",getMovies);
 
 export default router;
